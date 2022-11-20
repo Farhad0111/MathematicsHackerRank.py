e3111e8bb1ea38171baf24c68b3cc3fd5cb6326a
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2022-11-20 14:03:56 +0600

    Add files via upload

---
+++ Maximum Draws.py
@@ -0,0 +1,10 @@
+class Farhad:
+    def __init__(self,i):
+        self.i=i
+    def faru(self):
+        for j in range(0,self.i):
+            self.a=int(input())
+            print(self.a+1)
+farhad=Farhad(i=int(input()))
+farhad.faru()
+
