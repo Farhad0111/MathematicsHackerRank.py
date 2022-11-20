05aa0441921819fc93bbf1a0ec36bf9aab951f29
Farhad0111 <109299643+Farhad0111@users.noreply.github.com>
2022-11-20 15:28:48 +0600

    Add files via upload

---
+++ Handshake.py
@@ -0,0 +1,8 @@
+class Farhad:
+    def __init__(self):
+        i=int(input())
+        for j in range(0,i):
+            a=int(input())
+            print(int((a *(a- 1))/2))
+farhad=Farhad()
+farhad.__init__()
